91b9671a8cf02f0d49c94c0a52c9121b651045f2
Nikhil Sinha <nikhilsinha@outlook.com>
2018-11-08 19:56:51 -0800

    Player upload further updated

--- crawler/player_upload.bat
+++ crawler/player_upload.bat
@@ -7,7 +7,7 @@ dir /a:d /b > ../dirnames.txt 2>&1
 cd ..
 for /f "tokens=*" %%B in (dirnames.txt) do (
 dir .\player_jsons\%%B /b > .\player_jsons\filenames.txt 2>&1
-for /f "tokens=*" %%A in (.\player_jsons\filenames.txt) do mongoimport --host mongodb+srv://nsp-cluster-zqniz.mongodb.net/test --ssl --username %uname% --password %pass% --authenticationDatabase admin --db Players --collection %%B --type JSON --file player_jsons\%%B\%%A
+for /f "tokens=*" %%A in (.\player_jsons\filenames.txt) do mongoimport --uri mongodb+srv://%uname%:!pass!@nsp-cluster-zqniz.mongodb.net/Players --collection %%B --type json --file player_jsons\%%B\%%A
 del .\player_jsons\filenames.txt
 )
 del dirnames.txt
